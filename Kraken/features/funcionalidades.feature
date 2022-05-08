db1cf746b6f4cb20ca22ebb2e1888d9cf7298f3c
ChristianBorrasTorres <c.borras@uniandes.edu.co>
2022-05-08 08:41:51 -0500

    Se agregan los test de kraken con las funcionalidades correspondientes, debido a que no se puede ejecutar multiple archivos feature se debe realizar en un solo archivo las pruebas

---
+++ Kraken/features/funcionalidades.feature
@@ -0,0 +1,80 @@
+Feature: Crear una página
+
+@user1 @web
+Scenario: Como usuario inicio sesión y creo una página
+  Given I navigate to page "<PAGE>"
+  And I wait for 5 seconds
+  When I enter email "<USERNAME>"
+  Then I enter password "<PASSWORD>" 
+  And I wait for 2 seconds
+  Then I click on the login button
+  And I wait for 2 seconds
+  Then I click on pages
+  And I wait for 2 seconds
+  Then I click on new page
+  And I wait for 2 seconds
+  Then I enter a page title "<PAGETITLE>"
+  And I wait for 2 seconds
+  Then I click on 'Begin writing your page ...'
+  And I wait for 2 seconds
+  Then I enter a page content "<PAGECONTENT>"
+  And I wait for 10 seconds
+  Then I click on pages to return
+  And I wait for 5 seconds
+
+  Then I click on pages
+  And I wait for 2 seconds
+  Then I click on new page
+  And I wait for 2 seconds
+  Then I enter a page title "<PAGETITLE>"
+  And I wait for 2 seconds
+  Then I click on 'Begin writing your page ...'
+  And I wait for 2 seconds
+  Then I enter a page content "<PAGECONTENT>"
+  And I wait for 10 seconds
+  Then I click on publish button
+  Then I click on publish
+  And I wait for 5 seconds
+  Then I click on pages to return
+  And I wait for 5 seconds
+
+  Then I click on pages
+  And I wait for 2 seconds
+  Then I click on an existing page
+  And I wait for 2 seconds
+  Then I enter a page title "<UPDATEPAGETITLE>"
+  And I wait for 2 seconds
+  Then I click on 'Begin writing your page ...'
+  And I wait for 2 seconds
+  Then I enter a page content "<PAGECONTENT>"
+  And I wait for 10 seconds
+  Then I click on pages to return
+  And I wait for 5 seconds
+
+  Then I click on pages
+  And I wait for 2 seconds
+  Then I click on an existing page
+  And I wait for 2 seconds
+  Then I click on settings menu
+  And I wait for 2 seconds
+  Then I click on delete
+  And I wait for 2 seconds
+  Then I click on delete again
+  And I wait for 5 seconds
+
+  Then I click on post
+  And I wait for 2 seconds
+  Then I click on new post
+  And I wait for 2 seconds
+  Then I enter a post title "<POSTTITLE>"
+  And I wait for 2 seconds
+  Then I click on 'Begin writing your page ...'
+  And I wait for 2 seconds
+  Then I enter a post content "<POSTCONTENT>"
+  And I wait for 10 seconds
+  Then I click on post to return
+  And I wait for 5 seconds
+  Then I click on the user button
+  And I wait for 2 seconds
+  Then I click on sign out
+  And I wait for 5 seconds
--- Kraken/features/web/step_definitions/step.js
+++ Kraken/features/web/step_definitions/step.js
@@ -9,49 +9,186 @@ When('I navegate to page {kraken-string}', async function (page) {
 
 When('I enter email {kraken-string}', async function (email) {
 
-    let element = await this.driver.$('#email');
+    let element = await this.driver.$('.email');
 
     return await element.setValue(email);
 
 });
 
-When('I enter password {kraken-string}', async function (password) {
+Then('I enter password {kraken-string}', async function (password) {
 
-    let element = await this.driver.$('#pass');
+    let element = await this.driver.$('.password');
 
     return await element.setValue(password);
 
 });
 
 
-When('I click next', async function() {
+Then('I click on the login button', async function() {
 
-    let element = await this.driver.$('#loginbutton');
-
-    return await element.click();
-
-});
-
-Then('I click on the first conversation', async function () {
-
-    let element = await this.driver.$('.i224opu6 > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)');
+    let element = await this.driver.$('.login');
 
     return await element.click();
 
 });
 
-Then('I click on the redact message inputbox', async function () {
+Then('I click on pages', async function () {
 
-    let element = await this.driver.$('p.kvgmc6g5')
+    let element = await this.driver.$('a[href="#/pages/"]');
+
+    return await element.click();
+
+});
+
+Then("I click on new page", async function () {
+
+    let element = await this.driver.$("//a[@href='#/editor/page/']")
 
     return await element.click();
 
   });
 
   
-Then('I send the message', async function () {
+Then('I enter a page title {kraken-string}', async function (pageTitle) {
 
-    let element = await this.driver.$('span.tojvnm2t:nth-child(3) > div:nth-child(1)');
+    let element = await this.driver.$('.gh-editor-title');
+
+    return await element.setValue(pageTitle);
+
+  });
+
+
+Then("I click on 'Begin writing your page ...'", async function () {
+
+    let element = await this.driver.$(".koenig-editor")
+
+    return await element.click();
+
+  });
+
+Then('I enter a page content {kraken-string}', async function (pageContent) {
+
+    let element = await this.driver.$('.koenig-editor');
+
+    return await element.setValue(pageContent);
+
+  });
+
+Then("I click on pages to return", async function () {
+
+    let element = await this.driver.$(".gh-editor-back-button")
+
+    return await element.click();
+
+  });
+
+Then("I click on publish button", async function () {
+
+    let element = await this.driver.$(".gh-publishmenu")
+
+    return await element.click();
+
+  });
+
+Then("I click on publish", async function () {
+
+    let element = await this.driver.$(".gh-publishmenu-button")
+
+    return await element.click();
+
+  });
+
+Then("I click on the user button", async function () {
+
+    let element = await this.driver.$('div[role="button"]')
+
+    return await element.click();
+
+  });
+
+Then("I click on sign out", async function () {
+
+    let element = await this.driver.$('a[href="#/signout/"]')
+
+    return await element.click();
+
+  });
+
+  Then("I click on an existing page", async function () {
+
+    let element = await this.driver.$('.gh-post-list-title')
+
+    return await element.click();
+
+  });
+
+  Then("I click on settings menu", async function () {
+
+    let element = await this.driver.$('.settings-menu-toggle')
+
+    return await element.click();
+
+  });
+
+  Then("I click on delete", async function () {
+
+    let element = await this.driver.$('.gh-btn-hover-red')
+
+    return await element.click();
+
+  });
+
+  Then("I click on delete again", async function () {
+
+    let element = await this.driver.$('.gh-btn-red')
+
+    return await element.click();
+
+  });
+
+Then("I click on post", async function () {
+
+    let element = await this.driver.$('a[href="#/posts/"]')
+
+    return await element.click();
+
+  });
+
+Then("I click on new post", async function () {
+
+    let element = await this.driver.$('a[href="#/editor/post/"]')
+
+    return await element.click();
+
+  });
+
+Then('I enter a post title {kraken-string}', async function (postTitle) {
+
+    let element = await this.driver.$('.gh-editor-title');
+
+    return await element.setValue(postTitle);
+
+  });
+
+Then("I click on 'Begin writing your post ...'", async function () {
+
+    let element = await this.driver.$(".koenig-editor")
+
+    return await element.click();
+
+  });
+
+Then('I enter a post content {kraken-string}', async function (pageContent) {
+
+    let element = await this.driver.$('.koenig-editor');
+
+    return await element.setValue(pageContent);
+
+  });
+
+Then("I click on post to return", async function () {
+
+    let element = await this.driver.$('a[href="#/posts/"]')
 
     return await element.click();
 
