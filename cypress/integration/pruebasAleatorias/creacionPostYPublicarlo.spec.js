11bf3aff99c5901520c410de8d2311775ff40e99
Fredy Alejandro Russi Fajardo <f.russi@uniandes.edu.co>
2022-05-21 23:48:30 -0500

    Se agrega Xpath

---
+++ cypress/integration/pruebasAleatorias/creacionPostYPublicarlo.spec.js
@@ -0,0 +1,86 @@
+/// <reference types="cypress-xpath" />
+
+import PageObjectPost from "../../support/pageObjectPost";
+import {faker} from '@faker-js/faker'
+
+const pageObject = new PageObjectPost;
+const cookieSessionName = Cypress.env('cookieSessionName') || "ghost-admin-api-session"
+var i = 0;
+var caso = 4;
+var titleFake = '';
+var contentFake = '';
+
+context('Testing Create Post and Publish', () => { 
+
+    before(() => {
+        cy.visit('/#/signin');
+        cy.wait(3000);
+    })
+
+    beforeEach(() => {        
+        Cypress.Cookies.preserveOnce(cookieSessionName);
+        i = i +1;
+    })
+
+    after( () => {
+        cy.clearCookies();
+    })
+
+    it('1 Test Login into', () => {
+        pageObject.login();
+        cy.wait(3000);
+        cy.screenshot();
+    })
+
+    it('2 Test Page create post', () => {
+        pageObject.goToPosts();
+        cy.wait(2000); 
+        cy.screenshot();
+    })
+
+    it('3 Button new post', () => {
+        pageObject.selectNewPostButton();
+        cy.wait(2000);
+        cy.screenshot();
+    })
+
+    it('4 Test Enter Title', () => {
+        //pageObject.typeTitle();
+        titleFake = faker.name.jobTitle();
+        cy.get("textarea").first().type(titleFake);
+        //pageObject.typeContent();
+        contentFake = faker.lorem.paragraph();
+        cy.get(".koenig-editor__editor").type(contentFake);
+        cy.wait(2000);
+        cy.screenshot();
+    });       
+   
+    it('5 Test Publish', () => {     
+        pageObject.selectPublishButton();   
+        cy.wait(2000);
+        pageObject.publishPost();
+        cy.wait(3000);
+        pageObject.confirmPublishPost();
+        cy.wait(3000);
+        cy.screenshot();
+    });       
+   
+
+    it('6 Button Back to page', () => {
+        pageObject.goToPosts();
+        cy.screenshot();
+    });
+
+    it('7 Check post', () => {
+        cy.xpath('//h3[text()="'+titleFake+'"]').first().click();
+        cy.wait(3000);
+
+        /*cy.get('h3').then(($header)=>{
+            expect($header[0].innerText).to.equal(titleFake);
+        }
+        )*/
+    });
+
+
+     
+})
